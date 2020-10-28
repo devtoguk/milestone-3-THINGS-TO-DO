be26457cf690320d973b4b72799aa4bd0007e29d
devtoguk <devtoguk@gmail.com>
2020-10-28 22:17:11 +0000

    Install Flask setup and test basic route

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html",
+                           page_title="Things to Do and Places to Go: Home page",
+                           page_description="From small adventures at home, to big adventures on days out! Find something to do...")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
