98bfd77c2f796634c59b03018e9b8e77beffa413
devtoguk <devtoguk@gmail.com>
2020-10-30 21:56:04 +0000

    Setup and test the MongoDB connection
    Connect to the things_todo database and read from the activities collection.

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -13,6 +22,13 @@ def index():
     return render_template("index.html",
                            page_title="Things to Do and Places to Go: Home page",
                            page_description="From small adventures at home, to big adventures on days out! Find something to do...",nav_link="Home")
+
+
+@app.route("/dbtest")
+def dbtest():
+    activities = list(mongo.db.activities.find())
+    # print(activities)
+    return render_template("dbtest.html", activities=activities)
 
 
 if __name__ == "__main__":
