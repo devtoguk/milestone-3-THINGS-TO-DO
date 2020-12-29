ae2c1d5609035ee4fa58cc596c0c153b76411596
devtoguk <devtoguk@gmail.com>
2020-12-29 07:54:01 +0000

    Reformat code line

--- image.py
+++ image.py
@@ -30,7 +30,8 @@ def resize_image(image_file):
         scaled_height = int(image_height * scale_factor)
         offset_y = ((scaled_height - final_height) / 2)
 
-    box = (offset_x, offset_y, (final_width + offset_x), final_height + offset_y)
+    box = (offset_x, offset_y, (final_width + offset_x),
+           final_height + offset_y)
 
     new_image = image.resize((scaled_width, scaled_height)).crop(box)
     new_image.save(image_folder + image_file, 'JPEG')
