caa85e6adc501a47290687b33eb9d77c93910ee3
devtoguk <devtoguk@gmail.com>
2020-12-30 09:05:34 +0000

    Amend comments and refactor

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
-/**
- * Object to handle parts of the activity form
+/*
+ * Object to handle client-side activity form validation
  */
 const activityForm = {
     submitButtonText: '',
     imageUploadType: ['image/jpeg'],
     maxUploadImageSize: (4 * 1024 * 1024),
+    // Define fields to check and their error message
     checkFieldsEmpty: [
         {id: 'title', error: 'Field must be at least 4 characters long.'},
         {id: 'shortDescr', error: 'Field must be at least 4 characters long.'},
@@ -29,9 +30,9 @@ const activityForm = {
     },
 
     showFieldError: (formFieldID, errorMessage) => {
-        // remove previous error message
+        // Remove previous error message
         activityForm.clearFieldError(formFieldID);
-        // show new error message
+        // Show new error message
         $('#btn--form-update').text(activityForm.submitButtonText);
         $('#' + formFieldID + '--error').html('<span>' + errorMessage + '</span>');
         $('#' + formFieldID + '--error').removeClass('d-none');
@@ -41,19 +42,21 @@ const activityForm = {
 
     validateEmptyField: (formFieldID, errorMessage=' field error.') => {
         fieldValue = $('#' + formFieldID).val();
-        console.log('Field value: ', fieldValue)
+        // Check if field value blank or no selection made
         if (fieldValue == '' || fieldValue == 0) {
             activityForm.showFieldError(formFieldID, errorMessage)
             return true;
-        } else { return false; }
+        } else { 
+            return false;
+        }
     },
 
     validateFileInput: (formFieldID, fileSize, fileType) => {
         let fileError = false;
-        // clear existing file error and flash message
+        // Clear existing file error and flash message
         activityForm.clearFieldError(formFieldID);
         $('#flash--message').empty();
-        
+        // Check upload image size and type
         if (fileSize > activityForm.maxUploadImageSize) {
             let errorMessage = 'The selected file exceeds the upload file size limit';
             activityForm.showFieldError(formFieldID, errorMessage);
@@ -69,10 +72,8 @@ const activityForm = {
     },
 
     checkForm: () => {
-        console.log('JS validate form?');
         formError = false;
         activityForm.checkFieldsEmpty.forEach( (formField) => {
-            console.log(`Check field ID: ${formField.id} | Msg is: ${formField.error}`);
             if (activityForm.validateEmptyField(formField.id, formField.error)) {
                 formError = true;
             }
@@ -87,97 +88,82 @@ const activityForm = {
 };
 
 $('form[name=register_form').submit(function(e) {
+    e.preventDefault();
+    let $form = $(this);
+    let $error = $form.find('.form__error');
+    let data = $form.serialize();
 
-  var $form = $(this);
-  var $error = $form.find('.form__error');
-  var data = $form.serialize();
+    $.ajax({
+        url: '/user/add_user/',
+        type: 'POST',
+        data: data,
+        dataType: 'json',
+        success: function(resp) {
+            window.location.href = '/user/welcome/N/';
+        },
+        error: function(resp) {
+            $error.text(resp.responseJSON.error).removeClass('form__error-hidden');
+        }
+    });
 
-  $.ajax({
-    url: '/user/add_user/',
-    type: 'POST',
-    data: data,
-    dataType: 'json',
-    success: function(resp) {
-        window.location.href = '/user/welcome/N/';
-    },
-    error: function(resp) {
-        console.log(resp);
-        $error.text(resp.responseJSON.error).removeClass('form__error-hidden');
-    }
-  });
-
-  e.preventDefault();
 });
 
 $('form[name=login_form').submit(function(e) {
-
-  var $form = $(this);
-  var $error = $form.find('.form__error');
-  var data = $form.serialize();
-
-  $.ajax({
-    url: '/user/login_user/',
-    type: 'POST',
-    data: data,
-    dataType: 'json',
-    success: function(resp) {
-        window.location.href = '/user/welcome/R/';
-    },
-    error: function(resp) {
-        console.log(resp);
-        $error.text(resp.responseJSON.error).removeClass('form__error-hidden');
-    }
-  });
-
   e.preventDefault();
-});
+  let $form = $(this);
+  let $error = $form.find('.form__error');
+  let data = $form.serialize();
 
+    $.ajax({
+        url: '/user/login_user/',
+        type: 'POST',
+        data: data,
+        dataType: 'json',
+        success: function(resp) {
+            window.location.href = '/user/welcome/R/';
+        },
+        error: function(resp) {
+            $error.text(resp.responseJSON.error).removeClass('form__error-hidden');
+        }
+    });
 
-/* Show or hide the venue fields when Location is changed
-*/
-$('#location').change(function(e) {
-
-    const activityLocation = $('#location' ).val();
-    // const locationFields = ['name', 'postcode', 'address', 'email', 'website']
-    const locationFields = ['venue-name', 'venue-postcode', 'venue-address']
-    $('#venue-location').val(activityLocation);
-
-    if (activityLocation === '2') {
-        locationFields.forEach( field => {
-            $('#' + field).attr('required', '');
-        });
-        $('#venue--header').collapse('show');
-        $('#venue--details').collapse('show');
-    } else {
-        $('#venue--header').collapse('hide');
-        $('#venue--details').collapse('hide');
-        locationFields.forEach( field => {
-            $('#' + field).removeAttr('required');
-        });
-    }
-});
-
-/* Allow the user to toggle the venue details section
-*/
-$('#venue--toggle').click(function(e) {
-    $('#venue--details').collapse('toggle');
-});
-
-/* Activate tooltips
-*/
-$(function() {
-    $('[data-toggle="tooltip"]').tooltip()
 });
 
 $(document).ready(function(){
 
-    /* if on the activity_form
-    */
-   if( $('#title').length ) {
+    // If on the activity_form
+    if( $('#title').length ) {
+
+        // Activate tooltips
+        $('[data-toggle="tooltip"]').tooltip()
+
+        // Show or hide the venue fields when Location is changed
+        $('#location').change(function(e) {
+            const activityLocation = $('#location' ).val();
+            const locationFields = ['venue-name', 'venue-postcode', 'venue-address']
+            $('#venue-location').val(activityLocation);
+            if (activityLocation === '2') {
+                locationFields.forEach( field => {
+                    $('#' + field).attr('required', '');
+                });
+                $('#venue--header').collapse('show');
+                $('#venue--details').collapse('show');
+            } else {
+                $('#venue--header').collapse('hide');
+                $('#venue--details').collapse('hide');
+                locationFields.forEach( field => {
+                    $('#' + field).removeAttr('required');
+                });
+            }
+        });
+
+        // Allow the user to toggle the venue details section
+        $('#venue--toggle').click(function(e) {
+            $('#venue--details').collapse('toggle');
+        });
 
         /* Detect form submit to change activity button contents and
-        perform basic front-end validation.
-        */
+        perform basic front-end validation. */
         $('#btn--form-update').click(function(e) {
             image = $('#image').val();
             $('#btn--form-update').html('Processing <i class="fas fa-spinner fa-spin"></i>');
@@ -189,22 +175,19 @@ $(document).ready(function(){
         const currentLocation = $('#location' ).val()
         $('#venue-location').val(currentLocation);
 
-        /* re-show the venue fields if required on form-error re-display
-        */
+        // Re-show the venue fields if required on form-error re-display
         if (currentLocation == 2) { 
             $('#venue--header').collapse('show');
             $('#venue--details').collapse('show');
         }
 
-        /*  Reset form submit button to standard text
-        */
+        // Reset form submit button to standard text
         activityForm.submitButtonText = $('#btn--form-update').text();
         $('input, select, textarea').click(function(e) {
             $('#btn--form-update').text(activityForm.submitButtonText);
         });
 
-        /* Validate file input field on change
-        */
+        // Validate file input field on change
         $('#image').change( (e) => {
             let fileSize = e.originalEvent.target.files[0].size;
             let fileType = e.originalEvent.target.files[0].type;
@@ -216,14 +199,12 @@ $(document).ready(function(){
             activityForm.validateFileInput('image', fileSize, fileType);
         });
 
-        /* Remove field error on change
-        */
+        // Remove field error on change
         activityForm.checkFieldsEmpty.forEach( (formField) => {
             $('#' + formField.id).change( () => {
                 activityForm.clearFieldError(formField.id);
             });
         });
-        
     }
-});
 
+});
